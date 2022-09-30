231f652fc568ea97090018ae23b8fffb6ca8202a
atmathias <atmathias@gmail.com>
2022-09-30 06:38:07 +0300

    distance to health facility

--- R/data_processing.R
+++ R/data_processing.R
@@ -1,42 +1,43 @@
+# Summary statistics 2019
 
 # load library
 library(tidyverse)
 
-df_data_x <- readRDS("R/anon_module_x.rdata")
+df_data_x <- readRDS("inputs/anon_module_x.rdata")
 
-df_data_a <- readRDS("R/anon_module_a.rdata")
+df_data_a <- readRDS("inputs/anon_module_a.rdata")
 
-df_data_b <- readRDS("R/anon_module_b.rdata")
+df_data_b <- readRDS("inputs/anon_module_b.rdata")
 
-df_data_c <- readRDS("R/anon_module_c.rdata")
+df_data_c <- readRDS("inputs/anon_module_c.rdata")
 
-df_data_d <- readRDS("R/anon_module_d.rdata")
+df_data_d <- readRDS("inputs/anon_module_d.rdata")
 
-df_data_e <- readRDS("R/anon_module_e.rdata")
+df_data_e <- readRDS("inputs/anon_module_e.rdata")
 
-df_data_f <- readRDS("R/anon_module_f.rdata")
+df_data_f <- readRDS("inputs/anon_module_f.rdata")
 
-df_data_g1 <- readRDS("R/anon_module_g1.rdata")
+df_data_g1 <- readRDS("inputs/anon_module_g1.rdata")
 
-df_data_g2 <- readRDS("R/anon_module_g2.rdata")
+df_data_g2 <- readRDS("inputs/anon_module_g2.rdata")
 
-df_data_hi <- readRDS("R/anon_module_h1.rdata")
+df_data_hi <- readRDS("inputs/anon_module_h1.rdata")
 
-df_data_h2 <- readRDS("R/anon_module_h2.rdata")
+df_data_h2 <- readRDS("inputs/anon_module_h2.rdata")
 
-df_data_h3 <- readRDS("R/anon_module_h3.rdata")
+df_data_h3 <- readRDS("inputs/anon_module_h3.rdata")
 
-df_data_i <- readRDS("R/anon_module_i.rdata")
+df_data_i <- readRDS("inputs/anon_module_i.rdata")
 
-df_data_j <-  readRDS("R/anon_module_j.rdata")
+df_data_j <-  readRDS("inputs/anon_module_j.rdata")
 
-df_data_k <- readRDS("R/anon_module_k.rdata")
+df_data_k <- readRDS("inputs/anon_module_k.rdata")
 
-df_data_l <-  readRDS("R/anon_module_l.rdata")
+df_data_l <-  readRDS("inputs/anon_module_l.rdata")
 
-df_data_s <- readRDS("R/anon_module_s.rdata")
+df_data_s <- readRDS("inputs/anon_module_s.rdata")
 
-df_data_w <- readRDS("R/anon_module_w.rdata")
+df_data_w <- readRDS("inputs/anon_module_w.rdata")
 
 
 # Economic activity (per HH member)
@@ -70,7 +71,7 @@ host_summary_num_econ_activity_per_hh <- df_data_b %>%
 # Access to credit
 
 refugee_access_to_credit <- df_data_e %>%
-  left_join(df_data_b, by = "uhhidfs") %>% 
+  left_join(df_data_a, by = "uhhidfs") %>% 
   filter(!is.na(e3a), (a5b %in% c("Bidibidi_settlement", "Nakivale_settlement", "Palabeck_settlement", "Rhinocamp_settlement")
                        & hh_type == 1)) %>% 
   group_by(a5b, e3a) %>% 
@@ -81,7 +82,7 @@ refugee_access_to_credit <- df_data_e %>%
 
 
 host_access_to_credit <- df_data_e %>%
-  left_join(df_data_b, by = "uhhidfs") %>% 
+  left_join(df_data_a, by = "uhhidfs") %>% 
   filter(!is.na(e3a),(a2 %in% c("Yumbe", "Isingiro", "Lamwo", "Arua")
                       & hh_type == 1))%>%
   group_by(a2, e3a) %>% 
@@ -94,7 +95,7 @@ host_access_to_credit <- df_data_e %>%
 # Credit amount accessed in the past 12 months
 
 refugee_credit_amount_accessed <- df_data_e %>%
-  left_join(df_data_b, by = "uhhidfs") %>% 
+  left_join(df_data_a, by = "uhhidfs") %>% 
   filter((e3b > 50), (a5b %in% c("Bidibidi_settlement", "Nakivale_settlement", "Palabeck_settlement", "Rhinocamp_settlement")
                       & hh_type == 1)) %>% 
   group_by(a5b) %>% 
@@ -104,7 +105,7 @@ refugee_credit_amount_accessed <- df_data_e %>%
 
 
 host_credit_amount_accessed <- df_data_e %>% 
-  left_join(df_data_b, by = "uhhidfs") %>% 
+  left_join(df_data_a, by = "uhhidfs") %>% 
   filter((e3b > 50), (a2 %in% c("Yumbe", "Isingiro", "Lamwo", "Arua")
                       & hh_type == 0))%>% 
   group_by(a2) %>% 
@@ -135,6 +136,7 @@ host_highest_educ_level <- df_data_b %>%
     percentage_distribution = (hh_member_education_level/nrow(.))*100) %>% 
   arrange(b6)
 
+
 # hoh educ completed
 
 refugee_hoh_educ_level_completed <- df_data_b %>% 
@@ -155,6 +157,58 @@ host_hoh_educ_level_completed <- df_data_b %>%
     hoh_education_level = n(),
     percentage_distribution = (hoh_education_level/nrow(.))*100) %>% 
   arrange(b6)
+
+
+# hh trainings received
+
+
+#refugee_trainings_received <- df_data_hi %>%
+  #left_join(df_data_a, by = "uhhidfs") %>% 
+ # filter(!is.na(h2_1), (a5b %in% c("Bidibidi_settlement", "Nakivale_settlement", "Palabeck_settlement", "Rhinocamp_settlement")
+                        #& hh_type == 1))
+
+# Distance in km to public/private health facility
+# How do I eliminate outliers?
+
+refugee_distance_to_health_facility <- df_data_d %>% 
+  left_join(df_data_a, by = "uhhidfs") %>% 
+  filter(d1_3 != 999, (a5b %in% c("Bidibidi_settlement", "Nakivale_settlement", "Palabeck_settlement", "Rhinocamp_settlement")
+                      & hh_type == 1))%>% 
+  group_by(a5b) %>% 
+  summarise(
+    distance_to_health_facility = mean(d1_3, na.rm = TRUE) %>% 
+    round(1)) %>% 
+    arrange(desc(distance_to_health_facility))
+
+
+host_distance_to_health_facility <- df_data_d %>% 
+  left_join(df_data_a, by = "uhhidfs") %>% 
+  filter((d1_3 != 999 & d1_3 !=450), (a2 %in% c("Yumbe", "Isingiro", "Lamwo", "Arua")
+                       & hh_type == 0))%>% 
+  group_by(a2) %>% 
+  summarise(
+    distance_to_health_facility = mean(d1_3, na.rm = TRUE) %>% 
+      round(1)) %>% 
+  arrange(desc(distance_to_health_facility))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
